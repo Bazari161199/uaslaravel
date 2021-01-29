f42cc97d2f6ab7ea25da947b143072166a7c72c6
Bazari161199 <bazarinurmuhammad@gmail.com>
2021-01-29 09:00:01 +0700

    uaslaravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UAS</title>
+    <link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <section class="atas">
+            <div class="left">
+                <p>Letters Reported You: 0 / Amnesty International USA: 1,682</p>
+            </div>
+            <div class="right">
+                <ul class="nav justify-content-end">
+                    <li class="nav-item"><a href="/login">Log In</a></li>
+                    <li class="nav-item"><a href="/register">Sign Up</a></li>
+                </ul>
+            </div>
+                
+                
+           
+        </section>
+
+
+        <div class="pertama">
+            <div class="wrap">
+                <div class="kiri">
+                    <img src="./asset/w4r-logo.svg" alt="">
+                </div>
+                <div class="kanan">
+                    <nav class="nav">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" href="#">Intruction</a>
+                        <a class="nav-link" href="#">Classroom</a>
+                        <a class="nav-link" href="#">Event</a>
+                        <a class="nav-link" href="#">Report Your Letter</a>
+                        <a class="nav-link" href="#">Good News!</a>
+                        <a class="nav-link" href="#" style="color: yellow;">Donate</a>
+                      </nav>
+                </div>
+            </div>
+            <div class="bawah">
+                <div class="container">
+                    <div class="kiri">
+                        <p style="font-weight: bold;">WRITE A LETTER, CHANGE A LIFE</p>
+                        <p style="background-color: black; color: white; font-size: 18px;" >Write For Rights is Amnesty International’s largest annual human rights campaign. People like you around the world write letters on behalf of people who need urgent help. People like Nassima (pictured) who has been locked up since 2018 for protesting against the 'male guardianship' system in Saudi Arabia. Through the power of collective action, your letters will help convince government officials to free Nassima and other people unjustly imprisoned or facing abuses. Please sign up today!
+                            Questions? Email w4r@aiusa.org</p>
+                            <button type="button" class="btn btn-primary btn-lg">REPORT YOUR LETTERS</button>
+                            <a href="/register"><button type="button" class="btn btn-secondary btn-lg">SIGN UP</button></a>
+                    </div>
+                    <div class="kanan">
+                        <img src="./asset/Saudi-Arabia.jpg" alt="" style="width: 80%;">
+                        <p>Nassima has been jailed for defending women's rights
+                            TELL SAUDI ARABIA TO FREE NASSIMA NOW</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <section class="kotak">
+            <div class="konten">
+                <div class="img1" style="background: url(./asset/Burund.png);">
+                    <p style="margin-top: 350px;">JAILED FOR 32 YEAR FOR</p>
+                    <P>DEFENDING HUMAN RIGHTS</P>
+                    <p style="background-color: black; color: white; width: 100%;">Germain Rukuki / Burundi</p>
+
+                </div>
+
+                <div class="img1" style="background: url(./asset/Colombia.png);">
+                    <p style="margin-top: 350px;">THREATENED FOR DEFENDING THE</p>
+                    <p>AMAZON</p>
+                    <p style="background-color: black; color: white; width: 100%;">Jani Silva / Colombia</p>
+                </div>
+
+                <div class="img1" style="background: url(./asset/Pakistan.png);">
+                    <p style="margin-top: 350px;">ABDUCTED FOR EXPOSING</p>
+                    <p>DISSAPEARANCES</p>
+                    <p style="background-color: black; color: white; width: 100%;">Idris Khattak / Pakisatan</p>
+                </div>
+            </div>
+
+        </section>
+
+        <section class="last">
+            <div class="konten">
+                <div class="kiri" style="background: url(./asset/SSUDAN-scaled1.jpg);">
+                    </div>
+                <div class="kanan">
+                    <p style="background-color: white; font-size: 40px; color: black; font-weight: bold;">GOOD NEWS ON W4R CASES</p>
+                    <p style="font-size: 24px; font-weight: bold;">MAGAI MATIOP NGONG WAS REMOVED FROM DEATH ROW IS SOUTH SUDAN</p>
+                    <p style="font-size: 16px;">"...your colleagues have lighted up my life. Pass them all my regards and thanks.” Magai Matiop Ngong, Write for Rights 2019</p>
+                    <button type="button" class="btn btn-primary btn-lg" style="background-color: yellow; color: Black; border-color: yellow; border-radius: 0px; font-weight: bold;">MORE SUCCESS STORIES</button>
+                </div>
+            </div>
+        </section>
+
+        
+    </div>
+    <footer>
+        <div class="atas">
+            <div class="container">
+                <p>Questions? Contact Amnesty International USA's Write for Rights team: w4r@aiusa.org</p>
+            </div>
+        </div>
+        <div class="bawah">
+            <div class="container1">
+                <div class="kiri">
+                    <ul>
+                        <li><a class="nav-link" href="#">PRIVACY POLICY |</a></li>
+                        <li><a class="nav-link" href="#">TERM OF CONDITIONS</a></li>
+                    </ul>
+                    
+                    
+                    <p>© 2021 AMNESTY INTERNATIONAL USA</p>
+                </div>
+                <div class="kanan">
+                    <img src="./asset/facebook.svg" alt="">
+                    <img src="./asset/twitter.svg" alt="">
+                    <img src="./asset/instagram.svg" alt="">
+                    <img src="./asset/tumblr.svg" alt="">
+                    <img src="./asset/medium.svg" alt="">
+                    <p style="font-weight: bold; color: white; font-size: 24px;">#W4R2020</p>
+                    <img src="./asset/logo-footer.svg" alt="">
+                </div>
+            </div>
+        </div>    
+        
+    </footer>
+    
+    <script src="bootstrap.min.js"></script>
+    <script src="popper.min.js"></script>
+</body>
+</html>
